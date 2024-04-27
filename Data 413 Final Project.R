ad004f539d85da85037e1b49dae403f3241f7f41
Ethan-Pastel <143547475+Ethan-Pastel@users.noreply.github.com>
2024-04-26 20:06:03 -0400

    Add files via upload

--- Data 413 Final Project.R
+++ Data 413 Final Project.R
@@ -4,22 +4,28 @@ keywords <- c("Israel genocide", "Israel terrorism")
 gprop <- c("web")
 
 # Set other parameters
-geo <- "US"  # All regions
+states <- c(
+  "US-AL", "US-AK", "US-AZ", "US-AR", "US-CA", "US-CO", "US-CT", "US-DE", "US-FL", "US-GA",
+  "US-HI", "US-ID", "US-IL", "US-IN", "US-IA", "US-KS", "US-KY", "US-LA", "US-ME", "US-MD",
+  "US-MA", "US-MI", "US-MN", "US-MS", "US-MO", "US-MT", "US-NE", "US-NV", "US-NH", "US-NJ",
+  "US-NM", "US-NY", "US-NC", "US-ND", "US-OH", "US-OK", "US-OR", "US-PA", "US-RI", "US-SC",
+  "US-SD", "US-TN", "US-TX", "US-UT", "US-VT", "US-VA", "US-WA", "US-WV", "US-WI", "US-WY"
+)
 time <- "today 12-m"  # Last five years
 hl <- "en-US"  # Language
 tz <- 0  # Timezone
 
 # Perform the query
-get_trends <- function(keyword) {
-  trends <- gtrends(keyword = keyword,
-                    geo = geo,
+get_trends_state <- function(keyword) {
+  trends_state <- gtrends(keyword = keyword,
+                    geo = states,
                     time = time,
                     gprop = gprop,
                     hl = hl,
                     tz = tz)
-  return(trends)
+  return(trends_state)
 }
-
+print(get_trends_state)
 # View the results
 trends_data <- lapply(keywords, get_trends)
 
